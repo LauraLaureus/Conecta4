847e1f43e5d5a6343178a04d7819b387de9e69db
Laura <l4depende@gmail.com>
2015-04-16 13:36:51 +0100

    Primera aproximación de la clase Conecta4
    16/4/2015

--- conecta4.py
+++ conecta4.py
@@ -7,11 +7,20 @@ class Conecta4(Game):
 
     def __init__(self,filas=6,columnas=7,fichas=4):
         update(self,filas=filas,columna=columnas,fichas=fichas)
-        self.tablero = [range(numero_columnas) for i in range(numero_filas)]
+        movimientos = [(x, y) for x in range(1, filas+1)
+                 for y in range(1, columnas+1)]
+        self.inicial = Struct(to_move='X', utility=0, board={} moves=movimientos)
+        
         
     def legal_moves(self,state):
+        
 
     def make_move(self,move,state):
+        if move not in state.moves:
+            return state
+        board= state.board.copy()
+        #TODO: comprobar que la lista asociada a board[move] es menor que 6.
+        board[move].append(state.to_move)
 
     def utility(self,state,player):
 
